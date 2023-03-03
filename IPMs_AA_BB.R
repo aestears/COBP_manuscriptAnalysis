776d2921cdc6db974d8db4ad236c2327024ed224
aestears <astears@uwyo.edu>
2023-03-03 15:41:43 -0700

    updating IPM names to be consistent with manuscript

--- IPMs_AA_BB.R
+++ IPMs_AA_BB.R
@@ -3,47 +3,13 @@
 # Alice Stears
 # 08 December 2021
 #/////////////////////////
+#### load vital rate models from previous script ####
+source("./analysis_scripts/COBP_IPM_02_VitalRateModels.R")
 
-#### DI IPM, for each population ####
-### Define the demographic functions and parameters ###
-# models for each population
-# Soapstone data
-datSoap <- dat_all[dat_all$Location=="Soapstone",]
-survDat_soap <- datSoap[datSoap$flowering==0 | is.na(datSoap$flowering),]
-survMod_soap <- glm(survives_tplus1 ~ log_LL_t , data = survDat_soap, family = binomial)
-## Growth ($G(z',z)$)
-# lm w/ log-transformed size_t and size_t+1
-sizeMod_soap <- lm(log_LL_tplus1 ~ log_LL_t , data = datSoap)
-## Number of seeds produced, according to plant size ($b(z)$)
-seedDat_soap <- datSoap[datSoap$flowering == 1,]
-# fit a negative binomial glm (poisson was overdispersed)
-seedMod_soap <- MASS::glm.nb(Num_seeds ~ log_LL_t , data = seedDat_soap)
-## Flowering probability ($p_b(z)$)
-flwrMod_soap <- suppressWarnings((glm(flowering ~ log_LL_t + I(log_LL_t^2) , data = datSoap, family = binomial)))
-## Distribution of recruit size ($c_o(z')$)
-recD_soap <- datSoap[datSoap$seedling == 1,]
-recMod_soap <- lm(log_LL_t ~ 1, data = recD_soap)
-
-# FEWAFB data
-datBase <- dat_all[dat_all$Location=="FEWAFB",]
-survDat_Base <- datBase[datBase$flowering==0 | is.na(datBase$flowering),]
-survMod_Base <- glm(survives_tplus1 ~ log_LL_t , data = survDat_Base, family = binomial)
-## Growth ($G(z',z)$)
-# lm w/ log-transformed size_t and size_t+1
-sizeMod_Base <- lm(log_LL_tplus1 ~ log_LL_t , data = datBase)
-## Number of seeds produced, according to plant size ($b(z)$)
-seedDat_Base <- datBase[datBase$flowering == 1,]
-# fit a negative binomial glm (poisson was overdispersed)
-seedMod_Base <- MASS::glm.nb(Num_seeds ~ log_LL_t , data = seedDat_Base)
-## Flowering probability ($p_b(z)$)
-flwrMod_Base <- suppressWarnings((glm(flowering ~ log_LL_t + I(log_LL_t^2) , data = datBase, family = binomial)))
-## Distribution of recruit size ($c_o(z')$)
-recD_Base <- datBase[datBase$seedling == 1,]
-recMod_Base <- lm(log_LL_t ~ 1, data = recD_Base)
-
+#### vital rate functions #### 
+## same for both models 
 ## vital rate functions 
 # Construct an IPM kernel K using the parameters we obtained from the models
-
 # First define the elements that make up the IPM (vital rates):
 # SURVIVAL:
 S.fun <- function(z, paramCont) {
@@ -75,6 +41,28 @@ SDP.fun <- function(z, paramCont) {
   mu.fps=exp(paramCont$b_int + paramCont$b_slope *z)
   return(mu.fps)
 }
+
+#### IPM AA ####
+### DI IPM w/ all transitions for Soapstone Prairie ###
+### Define the demographic functions and parameters ###
+# models for each population
+# Soapstone data
+datSoap <- dat_all[dat_all$Location=="Soapstone",]
+survDat_soap <- datSoap[datSoap$flowering==0 | is.na(datSoap$flowering),]
+survMod_soap <- glm(survives_tplus1 ~ log_LL_t , data = survDat_soap, family = binomial)
+## Growth ($G(z',z)$)
+# lm w/ log-transformed size_t and size_t+1
+sizeMod_soap <- lm(log_LL_tplus1 ~ log_LL_t , data = datSoap)
+## Number of seeds produced, according to plant size ($b(z)$)
+seedDat_soap <- datSoap[datSoap$flowering == 1,]
+# fit a negative binomial glm (poisson was overdispersed)
+seedMod_soap <- MASS::glm.nb(Num_seeds ~ log_LL_t , data = seedDat_soap)
+## Flowering probability ($p_b(z)$)
+flwrMod_soap <- suppressWarnings((glm(flowering ~ log_LL_t + I(log_LL_t^2) , data = datSoap, family = binomial)))
+## Distribution of recruit size ($c_o(z')$)
+recD_soap <- datSoap[datSoap$seedling == 1,]
+recMod_soap <- lm(log_LL_t ~ 1, data = recD_soap)
+
 ## fit IPM for Soapstone data
 #plot the results
 # Empty list to save model coefficients 
@@ -135,13 +123,13 @@ Pkernel <- cbind(Pkernel.seedbank,rbind(rep(0,length(meshp)),Pkernel.cont)) # di
 Fkernel.cont <-  as.matrix(goCont * ((c_o) %*% diag(FecALL))) # the size of seedlings that go into the seed bank from each continuous size class
 Fkernel.discr  <- matrix(c(0, goSB * (FecALL)), nrow = 1)
 Fkernel <- rbind(Fkernel.discr, cbind(rep(0, length.out = n),Fkernel.cont))
-mat_all_Soap <-Pkernel+Fkernel
-lam_all_Soap <- eigen(mat_all_Soap)$values[1]
+mat_IPM_AA <-Pkernel+Fkernel
+lam_IPM_AA <- eigen(mat_IPM_AA)$values[1]
 
 ## bootstrap 95% CI
-soapDI_bootCI_lambdas <- numeric(1000L)
+IPM_AA_bootCI_lambdas <- numeric(1000L)
 # make a list to hold the model parameters
-soapDI_bootCI_params <- list()
+IPM_AA_bootCI_params <- list()
 # set seedbank parameters, which don't change
 outSB <- outSB_all #SB to continuous stage
 staySB <- staySB_all # staying in SB
@@ -238,15 +226,33 @@ for(i in 1:1000) {
   
   eigenMat <- base::eigen(mat)
   # save the lambda
-  soapDI_bootCI_lambdas[i] <- eigenMat$values[1]
+  IPM_AA_bootCI_lambdas[i] <- eigenMat$values[1]
   # save the parameters
-  soapDI_bootCI_params[[i]] <- paramCont_now
+  IPM_AA_bootCI_params[[i]] <- paramCont_now
 }
 
 ## calculate 95% CI for lambdas
-SE <- sd(soapDI_bootCI_lambdas)/sqrt(1000)
-mean <- mean(soapDI_bootCI_lambdas)
-soapDI_CI <- c((mean - 1.96*SE),(mean + 1.96*SE))
+SE <- sd(IPM_AA_bootCI_lambdas)/sqrt(1000)
+mean <- mean(IPM_AA_bootCI_lambdas)
+IMP_AA_CI <- c((mean - 1.96*SE),(mean + 1.96*SE))
+
+#### IPM BB ####
+# FEWAFB data
+datBase <- dat_all[dat_all$Location=="FEWAFB",]
+survDat_Base <- datBase[datBase$flowering==0 | is.na(datBase$flowering),]
+survMod_Base <- glm(survives_tplus1 ~ log_LL_t , data = survDat_Base, family = binomial)
+## Growth ($G(z',z)$)
+# lm w/ log-transformed size_t and size_t+1
+sizeMod_Base <- lm(log_LL_tplus1 ~ log_LL_t , data = datBase)
+## Number of seeds produced, according to plant size ($b(z)$)
+seedDat_Base <- datBase[datBase$flowering == 1,]
+# fit a negative binomial glm (poisson was overdispersed)
+seedMod_Base <- MASS::glm.nb(Num_seeds ~ log_LL_t , data = seedDat_Base)
+## Flowering probability ($p_b(z)$)
+flwrMod_Base <- suppressWarnings((glm(flowering ~ log_LL_t + I(log_LL_t^2) , data = datBase, family = binomial)))
+## Distribution of recruit size ($c_o(z')$)
+recD_Base <- datBase[datBase$seedling == 1,]
+recMod_Base <- lm(log_LL_t ~ 1, data = recD_Base)
 
 ## fit IPM for Base data
 #plot the results
@@ -305,13 +311,13 @@ Fkernel.cont <-  as.matrix(paramsBase$goCont * ((c_o) %*% diag(FecALL))) # the s
 Fkernel.discr  <- matrix(c(0, paramsBase$goSB * (FecALL)), nrow = 1)
 Fkernel <- rbind(Fkernel.discr, cbind(rep(0, length.out = n),Fkernel.cont))
 
-mat_all_Base <-Pkernel+Fkernel
-lam_all_Base <- eigen(mat_all_Base)$values[1]
+mat_IPM_BB <-Pkernel+Fkernel
+lam_IPM_BB <- eigen(mat_IPM_BB)$values[1]
 
 ## bootstrap 95% CI
-baseDI_bootCI_lambdas <- numeric(1000L)
+IPM_BB_bootCI_lambdas <- numeric(1000L)
 # make a list to hold the model parameters
-baseDI_bootCI_params <- list()
+IPM_BB_bootCI_params <- list()
 # set seedbank parameters, which don't change
 outSB <- outSB_all #SB to continuous stage
 staySB <- staySB_all # staying in SB
@@ -408,12 +414,12 @@ for(i in 1:1000) {
   
   eigenMat <- base::eigen(mat)
   # save the lambda
-  baseDI_bootCI_lambdas[i] <- eigenMat$values[1]
+  IPM_BB_bootCI_lambdas[i] <- eigenMat$values[1]
   # save the parameters
-  baseDI_bootCI_params[[i]] <- paramCont_now
+  IPM_BB_bootCI_params[[i]] <- paramCont_now
 }
 
 ## calculate 95% CI for lambdas
-SE <- sd(baseDI_bootCI_lambdas)/sqrt(1000)
-mean <- mean(baseDI_bootCI_lambdas)
+SE <- sd(IPM_BB_bootCI_lambdas)/sqrt(1000)
+mean <- mean(IPM_BB_bootCI_lambdas)
 baseDI_CI <- c((mean - 1.96*SE),(mean + 1.96*SE))
